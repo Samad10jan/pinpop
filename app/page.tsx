a20589ef13ecc2e7456e6dc588e40619f2d7ff51
Samad10jan <abdulsamad10jan@gmail.com>
2026-02-07 20:03:30 +0530

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,99 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <main className="page">
+
+      <header className="sticky flex top-3 justify-between mx-8 mt-5  ">
+
+        <div className="flex gap-4 *:p-8">
+          <button className="btn-circle bg-[#EA2E00] ">Logo</button>
+          <button className="btn-circle bg-[#3eea00ac]">Join</button>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+
+        <button className="btn-circle bg-[#ea0090] p-8 ">Login</button>
+
+      </header>
+
+      <h1 className="text-center text-[96px] font-extrabold sticky -top-6 ">PIN-POP</h1>
+
+     
+      <section className="container flex justify-center gap-15 my-80">
+
+        <div className="flex flex-col justify-between gap-10 ">
+
+          <div className="image-card hero-wide bg-[#EA2E00] text-white">
+            Image
+          </div>
+
+          <div className="card hero-text">
+            Image and from image something about ppinterest
+          </div>
+
         </div>
-      </main>
-    </div>
+
+        <div className="flex flex-col justify-between gap-10">
+
+          <div className="card hero-text">
+            Image and from image something about pinterest
+          </div>
+
+          <div className="image-card hero-big bg-[#FE7F2D] text-white">
+            Image
+          </div>
+
+        </div>
+
+      </section>
+
+      <section className="gallery">
+
+        <div className="container  flex gap-20">
+
+          <div className="image-card h-130 w-[35%]  bg-[#FE7F2D] text-white">
+            Image
+          </div>
+
+          <div className=" flex flex-1 flex-col gap-10">
+
+            <div className="image-card h-50 bg-[#FE7F2D] text-white">
+              Image
+            </div>
+
+            <div className=" flex! flex-!col justify-between h-full gap-8 ">
+
+              <div className="image-card bg-[#FE7F2D] flex-2  text-white">Image</div>
+              <div className="image-card bg-[#FE7F2D] flex-1 text-white">Image</div>
+
+            </div>
+
+          </div>
+
+        </div>
+
+      </section>
+
+      
+      <section className="cta">
+
+        <div className="container flex justify-center gap-50">
+
+          <div className="cta-circle">Image</div>
+
+          <div className="self-center">
+
+            <div className="card mb-8 ">
+              Image at left and some quote and say to join
+            </div>
+
+            <button className="btn-rect bg-[#3eea00ac]">JOIN</button>
+
+          </div>
+
+        </div>
+
+      </section>
+
+      <footer className="footer" />
+
+    </main>
   );
 }
